12922c9af7d98d3e4be04ecd1dcded751935a71f
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2022-03-29 17:37:39 +0200

    make base tmm and adm importable modules

--- src/anomaly/__init__.py
+++ src/anomaly/__init__.py
@@ -6,4 +6,8 @@ Code for the 2021 MVA course 'ML for time series'
 
 from __future__ import annotations
 
+import anomaly.adm as adm  # noqa
+import anomaly.base as base  # noqa
+import anomaly.tmm as tmm  # noqa
+
 __version__ = "0.1"
