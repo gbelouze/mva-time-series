d2652165ced88d84060a8007a1c6614525d8ae03
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2022-03-30 20:10:27 +0200

    expose all predictors

--- src/anomaly/tmm/__init__.py
+++ src/anomaly/tmm/__init__.py
@@ -1,5 +1,19 @@
 from .ARIMA import AR as AR  # noqa
 from .ARIMA import ARMA as ARMA  # noqa
 from .ARIMA import MA as MA  # noqa
+from .merge import Best as Best  # noqa
+from .merge import Sequential as Sequential  # noqa
 from .NaivePredictor import NaivePredictor as NaivePredictor  # noqa
 from .Polynomial import Polynomial as Polynomial  # noqa
+from .Trigonometric import Trigonometric as Trigonometric  # noqa
+
+all_predictors = {
+    "ar": AR,
+    "ma": MA,
+    "arma": ARMA,
+    "naive": NaivePredictor,
+    "polynomial": Polynomial,
+    "trigonometric": Trigonometric,
+    "poly+arma": lambda: Sequential(predictors=[Polynomial(), ARMA()]),
+    "poly+trigo+arma": lambda: Sequential(predictors=[Polynomial(), Trigonometric(), ARMA()]),
+}
