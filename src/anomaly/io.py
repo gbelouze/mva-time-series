5e92df8b3cfc9e75f96de6db5d84fb39a27f5147
Clément Ogier <64908052+clogier@users.noreply.github.com>
2022-03-30 09:53:02 +0200

    created benchmark class

--- src/anomaly/io.py
+++ src/anomaly/io.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import glob
 
 import pandas as pd  # type: ignore
 
@@ -16,3 +17,21 @@ def read(benchmark_index: int, dataset_index: int) -> pd.DataFrame:
     benchmark = data_dir / f"A{benchmark_index}Benchmark"
     base = benchmarks[benchmark]
     return pd.read_csv(benchmark / f"{base}{dataset_index}.csv")
+
+
+class BenchmarkDataset():
+    def __init__(self, benchmark_index : int):
+        benchmark = data_dir / f"A{benchmark_index}Benchmark"
+        base = benchmarks[benchmark]
+
+        s = str(benchmark / f"{base}*.csv")
+        self.files = glob.glob(s)
+        self.len = len(self.files)
+
+    def read(self, i : int):
+        if i < 0 or i >= self.len:
+            raise IndexError('index out of dataset')
+        df = pd.read_csv(self.files[i])
+        df = df.rename(columns={"anomaly": "is_anomaly", "timestamps": "timestamp"})
+
+        return df
